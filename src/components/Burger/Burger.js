0c35313e1ec15943f7c02ef0221eb58a9ea531f9
octoccoper <octocopper@gmail.com>
2021-02-23 23:38:06 +0200

    adding BuildControl, BuilderControls components

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,15 +4,25 @@ import classes from './Burger.css';
 
 const burger = ( props ) => {
 
-    const transformedIngridients = Object.keys(props.ingredients)
+    let transformedIngridients = Object.keys(props.ingredients)
     .map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_,i) => {
-            <BurgerIngredient key={igKey + i} type={igKey} />
+            return <BurgerIngredient key={igKey + i} type={igKey} />
         });
-    })
+    });
+
+    let ingredientsArr = transformedIngridients.reduce((accumulator, currentValue) => {
+        return accumulator.concat(currentValue);
+    });
+
+    if(ingredientsArr.length === 0) {
+        transformedIngridients = (<p>Please add any ingredients</p>)
+    }
+
         return (
             <div className={classes.Burger}>
                 <BurgerIngredient type="bread-top" />
+                {transformedIngridients}
                 <BurgerIngredient type="bread-bottom" />
             </div>
         );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
 class BurgerBuilder extends Component {
 
 state = {
     ingredients: {
         cheese: 2,
-        salad: 3,
+        salad: 1,
         bacon: 1,
         meat: 2
     }
@@ -16,10 +17,8 @@ state = {
     render() {
         return (
             <Aux>
-                <div>
-                    <Burger ingredients={this.state.ingredients}/>
-                </div>
-                <div>Build Controls</div>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls/>
             </Aux>
         );
     }
