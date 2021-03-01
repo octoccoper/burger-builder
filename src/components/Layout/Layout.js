61c0bc0a778a8f96295b02ca8b5cbd29a4aeab62
octoccoper <octocopper@gmail.com>
2021-03-01 15:29:49 +0200

    updating favicon; changing login for SideDrawer in Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <Aux>
-        <SideDrawer />
-        <Toolbar />
-        <main className={classes.Content}>
-            { props.children }
-            
-        </main>
-    </Aux>
-); 
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-export default layout;
+    sideDrawerCloseHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        });
+    }
+
+    render () {
+        return (
+            <Aux>
+                <SideDrawer 
+                    open={this.state.showSideDrawer} 
+                    closed={this.sideDrawerCloseHandler}/>
+
+                <Toolbar />
+
+                <main className={classes.Content}>
+                    { this.props.children }               
+                </main>
+            </Aux>
+        )
+    }
+}
+
+
+export default Layout;
